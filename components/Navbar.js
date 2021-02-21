bd2260888e3ed3ded92e3caa1fb5bb3e72c7f330
Rohankumar Maisuria <rmaisuria@horizon.csueastbay.edu>
2021-02-21 20:12:46 +0530

    updated next

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,18 +1,16 @@
 import Link from 'next/link';
 
 const Navbar = () => {
-  return (  
-    <div>
-      <nav>
+  return (
+    <nav>
       <div className="logo">
         <h1>Ninja List</h1>
       </div>
       <Link href="/"><a>Home</a></Link>
       <Link href="/about"><a>About</a></Link>
       <Link href="/list"><a>List</a></Link>
-      </nav>
-    </div>
-  ); 
+    </nav>
+  );
 }
 
 export default Navbar;
